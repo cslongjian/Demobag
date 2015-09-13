f56404214f16c1f8cc36a013008567bff090a0f0
cslongjian <504768527@qq.com>
2015-09-13 17:11:24 +0800

    VC生命周期

--- TestDemo/TestDemo/Class/Controller/LifeCycleOfViewController.m
+++ TestDemo/TestDemo/Class/Controller/LifeCycleOfViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    NSLog(@"1控制器的view加载完毕----viewDidLoad");
     // Do any additional setup after loading the view.
 }
 
@@ -33,5 +34,49 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+// 控制器的view即将显示的时候调用
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:YES];
+    NSLog(@"1控制器的view即将显示----viewWillAppear");
+}
+// 控制器的view完全显示的时候调用
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSLog(@"1控制器的view完全显示----viewDidAppear");
+}
+
+// 控制器的view即将消失的时候调用
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:animated];
+    NSLog(@"1控制器的view即将消失----viewWillDisappear");
+}
+// 控制器的view完全消失的时候调用
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:animated];
+    NSLog(@"1控制器的view完全消失---viewDidDisappear");
+}
+
+// 控制器的view即将销毁的时候调用
+- (void)viewWillUnload
+{
+    [super viewWillUnload];
+    NSLog(@"1控制器的viewWillUnload");
+}
+// 控制器的view完全销毁的时候调用
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    NSLog(@"1控制器的viewDidUnload");
+    // 清空不需要的属性
+    //    [self.foods release];
+    //         self.foods = nil;
+}
+
 
 @end
