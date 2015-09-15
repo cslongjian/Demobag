4beaa0ceb010c2486cb546430dd828bbf62e0533
cslongjian <504768527@qq.com>
2015-09-15 16:54:24 +0800

    使用CGRECT方法获取相应的宽高

--- AutoLayout/AutoLayout/ViewController.m
+++ AutoLayout/AutoLayout/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    UIView *_uivew1;
+    UIView *_uivew2;
+}
 
 @end
 
@@ -17,6 +21,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _uivew1 = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 40)];
+    _uivew1.backgroundColor = [UIColor blueColor];
+    
+    //距离1 下边10位置添加一个2 距离左右为5.
+    _uivew2 = [[UIView alloc]initWithFrame:CGRectMake(5, CGRectGetHeight(_uivew1.frame)+10, _uivew1.frame.size.width-10, _uivew1.frame.size.height)];
+    _uivew2.backgroundColor = [UIColor redColor];
+    
+    
+    [self.view addSubview:_uivew1];
+    [self.view addSubview:_uivew2];
 }
 
 - (void)didReceiveMemoryWarning {
